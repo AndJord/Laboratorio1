841d3f58d5f70fe096d22d4640b8fc6a7f5c661b
Sergio Lopez Morales <sergiolopezm098@gmail.com>
2022-09-10 19:14:25 -0600

    Se agregan los atributos, metodos de acceso y metodo constructor

--- src/lab1/CuentaDolares.java
+++ src/lab1/CuentaDolares.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab1;
 
 /**
@@ -9,5 +6,38 @@ package lab1;
  * @author ADMIN
  */
 public class CuentaDolares {
+    private Integer numeroCuenta;
+    private double saldoCliente;
+    private double tipoCambio;
+
+    public Integer getNumeroCuenta() {
+        return numeroCuenta;
+    }
+    
+    
+
+    public double getSaldoCliente() {
+        return saldoCliente;
+    }
+
+    public void setSaldoCliente(double saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
+
+    public double getTipoCambio() {
+        return tipoCambio;
+    }
+
+    public void setTipoCambio(double tipoCambio) {
+        this.tipoCambio = tipoCambio;
+    }
+
+    public CuentaDolares(Integer numeroCuenta, double saldoCliente, double tipoCambio) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldoCliente = saldoCliente;
+        this.tipoCambio = tipoCambio;
+    }
+    
+    
     
 }
