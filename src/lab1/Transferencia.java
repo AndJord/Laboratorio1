673fac1487bed43eace167e658cffc3ab21cb0c0
Andres Mejias <anmejiasma@est.utn.ac.cr>
2022-09-10 23:06:38 -0600

    Se crea la clase Transferencia

---
+++ src/lab1/Transferencia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1;
+
+/**
+ *
+ * @author andre
+ */
+public class Transferencia extends Transacciones {
+
+    public Transferencia(int Id, String cuentaorigen, String cuentadestino, double monto, double saldoanterior, double saldofinal, double saldoanteriordestino, double saldofinaldestino) {
+        super(Id, cuentaorigen, cuentadestino, monto, saldoanterior, saldofinal, saldoanteriordestino, saldofinaldestino);
+    }
+
+    public void Transaccion() {
+
+    }
+
+}
