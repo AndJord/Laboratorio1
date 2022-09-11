def272640f0e5ce0025d9127072f017c085dcd58
Joyce Francela Useda Medrano <jousedame@est.utn.ac.cr>
2022-09-10 19:28:48 -0600

    Se agregan atributos, metodos de acceso y metodo constructor

--- src/lab1/TarjetaCredito.java
+++ src/lab1/TarjetaCredito.java
@@ -9,5 +9,49 @@ package lab1;
  * @author Joyce
  */
 public class TarjetaCredito {
+    //Atributos
+   protected int numeroTarjeta;    
+   protected String cuenta;
+   protected String fechaCorte;
+   protected Double limite;
+   protected Double saldo;
+   
+   //Metodos
+
+    public int getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public String getFechaCorte() {
+        return fechaCorte;
+    }
+
+    public void setFechaCorte(String fechaCorte) {
+        this.fechaCorte = fechaCorte;
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
     
+    //Constructor
+
+    public TarjetaCredito(int numeroTarjeta, String cuenta, String fechaCorte, Double limite, Double saldo) {
+        this.numeroTarjeta = numeroTarjeta;
+        this.cuenta = cuenta;
+        this.fechaCorte = fechaCorte;
+        this.limite = limite;
+        this.saldo = saldo;
+    }
+    
+
+   
 }
