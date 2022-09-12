a5fb1726b2d40dade5782cccf9194a31096993c1
Sergio Lopez Morales <sergiolopezm098@gmail.com>
2022-09-11 21:06:46 -0600

    Se agrega el metodo identificarCuentas

--- src/lab1/CuentaColones.java
+++ src/lab1/CuentaColones.java
@@ -1,30 +1,43 @@
 
 package lab1;
 
+import java.util.Objects;
+
 /**
  *
  * @author ADMIN
  */
 public class CuentaColones {
     private Integer numeroCuenta;
-    private double saldoCliente;
+    private float saldo;
 
     public Integer getNumeroCuenta() {
         return numeroCuenta;
     }
 
-    public double getSaldoCliente() {
-        return saldoCliente;
+    public double getSaldo() {
+        return saldo;
     }
 
-    public void setSaldoCliente(double saldoCliente) {
-        this.saldoCliente = saldoCliente;
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
     }
 
-    public CuentaColones(Integer numeroCuenta, double saldoCliente) {
+   
+
+    public CuentaColones(Integer numeroCuenta, float saldo) {
         this.numeroCuenta = numeroCuenta;
-        this.saldoCliente = saldoCliente;
+        this.saldo = saldo;
     }  
+    
+    public boolean identificarCuenta(Object cuenta){
+        if(this==cuenta) return true;
+        if(cuenta==null || getClass() != cuenta.getClass()) return false;
+        CuentaColones numeroC = (CuentaColones) cuenta;
+        return Float.compare(numeroC.saldo, saldo)== 0 &&
+                Objects.equals(numeroCuenta,numeroC.numeroCuenta);
+            
+    }
     
 }
 
