959fa9574b7a2de46ad6fd3bcac7d1c35e8687fa
Joyce Francela Useda Medrano <jousedame@est.utn.ac.cr>
2022-09-18 18:44:54 -0600

    Se crea la clase tarjeta que va a heredar a TarjetaCredito y TarjetaDebito

--- src/Tarjetas/Tarjeta.java
+++ src/Tarjetas/Tarjeta.java
@@ -7,6 +7,27 @@ package Tarjetas;
  */
 public abstract class Tarjeta {
     //Atributos
+    protected int numeroTarjeta;
+    protected String cuenta;
+    protected Tarjeta tarjetaDebito ;
+    
+    //Metodos
+
+    public int getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    
+    //Constructor
+
+    public Tarjeta(int numeroTarjeta, String cuenta) {
+        this.numeroTarjeta = numeroTarjeta;
+        this.cuenta = cuenta;
+    }
     
     
 }
